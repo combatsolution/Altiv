a959c019f2ecbf65601b3cac60b0433ac8371569
RakhKaran <132744751+RakhKaran@users.noreply.github.com>
2025-06-06 20:02:03 +0530

    "mobile header updated"

--- src/layouts/main/header.js
+++ src/layouts/main/header.js
@@ -204,7 +204,7 @@ export default function Header() {
                 )}
 
                 {user &&
-                  !isLoginPage && ( // Hide avatar on login page
+                  (!isLoginPage || !isProfilePage) && ( // Hide avatar on login page
                     <IconButton
                       component={m.button}
                       whileTap="tap"
@@ -232,9 +232,16 @@ export default function Header() {
                         }}
                       />
                     </IconButton>
-                  )}
+                  ) 
+                  }
+
+                  {isProfilePage && <Button variant="none" onClick={() => handleSignout()} sx={{ mr: 1,  color: "#0040D8", }}>
+                      Sign out
+                    </Button>}
 
                 {!user && <LoginButton />}
+
+                {}
 
                 {!mdUp && <NavMobile offsetTop={offsetTop} data={navConfig} />}
               </Stack>
